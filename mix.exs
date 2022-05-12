b1a2b000e082bc99e0529d87b41171100f2122c3
Mehmet Cetin <mcetin.cm@gmail.com>
2022-05-12 18:57:00 +0000

    bumps up credo version

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Indicado.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
